48d41dc5c4715205e3a78ade944451e7d32b3a10
smnmyr <simon.mayer@unisg.ch>
2022-03-09 09:33:02 +0100

    Add kick-off code for GraphDB

---
+++ graphDBstarter/simpleGraphQuery.py
@@ -0,0 +1,44 @@
+import requests
+import urllib
+from requests.exceptions import HTTPError
+import csv
+graph_address = "https://graphdb.interactions.ics.unisg.ch/repositories/was-assignment-3?query="
+
+user ="was-students"
+passcode = "assignment-3-is-fun"
+
+def readfile(filename):
+    f = open(filename,"r")
+    string_query = f.read()
+    return string_query
+
+def print_query_results(sparql_query_results):
+    reader = csv.reader(sparql_query_results.splitlines(), delimiter=';')
+    for row in reader:
+        for r in row:
+            fields = r.split(',')
+            for f in fields:
+                print("Fields: ", f)
+                
+def execute_SPARQL_Query(string_query): 
+    encoded_query = urllib.parse.quote_plus(string_query)
+    query = graph_address + encoded_query
+    response = requests.get(query, auth=(user, passcode), verify=True)
+    try:
+        print('Success executing query! \n')   
+        sparql_query_results = response.text
+        return sparql_query_results   
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+    except Exception as err:
+        print(f'Other error occurred: {err}')
+
+
+def main():
+    fileName = input("Name of the SPARQL Query file: ")
+    string_query = readfile(fileName)
+    sparql_query_results = execute_SPARQL_Query(string_query)
+    print_query_results(sparql_query_results)
+
+if __name__ == "__main__":
+    main()
